fd9ecb8e4f9a0a16d07158c56f203054b80be1e1
codejlay <jlay@outlook.com>
2015-10-08 17:09:25 -0700

    added scss, variable, fonts, mixin, reset

--- gulpfile.js
+++ gulpfile.js
@@ -1,84 +1,23 @@
-// var gulp = require('gulp');
-// var uglify = require('gulp-uglify');
-// var concat = require('gulp-concat');
-// // var connect = require('gulp-connect');
-// var browserSync = require('browser-sync').create();
+'use strict';
 
-// gulp.task('compress-js', function(){
-//   // place code for your default task here
-  
-//   	gulp.src('js/*.js')
-// 	  .pipe(concat('main.min.js'))
-// 	  .pipe(uglify())
-// 	  .pipe(gulp.dest('build/js'))
-
-// 	gulp.src('css/*.css')
-// 	.pipe(concat('main.css'))
-// 	.pipe(gulp.dest('build/css'))
-// });
-
-// // gulp.task('connect', function() {
-// // 	connect.server();
-// // });
-// gulp.task('browser-sync', function() {
-//     browserSync.init({
-//         server: {
-//             baseDir: "./"
-//         }
-//     });
-
-// 	gulp.watch(['js/*.js'], ['compress-js'])
-// 	gulp.watch(['js/*.js'].on('change'. browserSync.reload);
-// 	gulp.watch(['css/*.css'].on('change'. browserSync.reload);
-// 	// gulp.watch(['css/*.css'], ['compress-css'])
-// });
-
-
-
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
+var gulp = require('gulp'); // Load Gulp!
 var browserSync = require('browser-sync').create();
-var connect = require('gulp-connect');
+var sass = require('gulp-sass');
 
-gulp.task('compress-js', function()
-  {
-    gulp.src('js/*.js') // What files do we want gulp to consume?
-        .pipe(concat('main.min.js'))
-        .pipe(uglify()) // Call the uglify function on these files
-        .pipe(gulp.dest('build/js')) // Where do we put the result?
-  });
+gulp.task('scss', function () {
+  gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
 
-// Static server
-
-gulp.task('browser-sync', ['compress-js'], function() {
+gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
             baseDir: "./"
         }
-
     });
 
-gulp.watch('js/*.js', ['compress-js']);
-gulp.watch('js/*.js').on('change', browserSync.reload);
-gulp.watch('css/*.css').on('change', browserSync.reload);
+
+    gulp.watch('./scss/**/*.scss', ['scss']);
+    gulp.watch(["index.html", "js/*.js", "css/*.css"]).on('change', browserSync.reload);
 });
-
-// gulp.task('connect', function()
-
-//   {
-
-//     connect.server();
-
-//   });
-
-
-
-
-
-
-
-
-
-
-
